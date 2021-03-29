8388260724e02554bda004bd9fd054535a6a6999
jackdenkin1374 <kevincheng137@gmail.com>
2021-03-29 19:30:49 -0400

    Addition of Homestead Chronicles
    Added footer and tab javascript

---
+++ base-site.js
@@ -0,0 +1,8 @@
+var $ = function (id) { return document.getElementById(id); };
+
+window.onload = function () {
+    // Scripts from https://jqueryui.com/ 
+    $("#tabs").tabs();
+    $("#tabs2").tabs();
+    $("#tabs3").tabs();
+};
